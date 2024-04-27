5b4835c4480a8ec8b7bcda2ee861b67cf2529e15
Carlos Torres <ctorresmoral@gmail.com>
2024-04-27 20:23:50 +0200

    done

--- javascript/index.js
+++ javascript/index.js
@@ -4,37 +4,155 @@
 // 🚨🚨🚨 Comment out the below code before you start working on the code
 
 // Out of sync
-  getInstruction("mashedPotatoes", 0, (step1) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 1, (step2) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 2, (step3) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 3, (step4) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 4, (step5) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
-    document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
-  }, (error) => console.log(error));
+// getInstruction("mashedPotatoes", 0, (step1) => {
+//   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+// }, (error) => console.log(error));
 
+// getInstruction("mashedPotatoes", 1, (step2) => {
+//   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
+// }, (error) => console.log(error));
 
+// getInstruction("mashedPotatoes", 2, (step3) => {
+//   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
+// }, (error) => console.log(error));
+
+// getInstruction("mashedPotatoes", 3, (step4) => {
+//   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
+// }, (error) => console.log(error));
+
+// getInstruction("mashedPotatoes", 4, (step5) => {
+//   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
+//   document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
+// }, (error) => console.log(error));
 
 // Iteration 1 - using callbacks
-// ...
+const getRecipeMethods = (name, successText) => {
+  const recipeSteps = document.querySelector(`#${name}`)
+  const recipeImg = document.querySelector(`#${name}Img`)
+  const addStep = (stepText) =>
+    (recipeSteps.innerHTML += `<li>${stepText}</li>`)
+  const addSuccessText = () => addStep(successText)
+  const showImage = () => recipeImg.removeAttribute("hidden")
+  return {
+    name,
+    addStep,
+    addSuccessText,
+    showImage,
+  }
+}
+
+const recipe1 = getRecipeMethods("mashedPotatoes", "Mashed potatoes are ready!")
+getInstruction(
+  recipe1.name,
+  0,
+  (step) => {
+    recipe1.addStep(step)
+    getInstruction(
+      recipe1.name,
+      1,
+      (step) => {
+        recipe1.addStep(step)
+        getInstruction(
+          recipe1.name,
+          2,
+          (step) => {
+            recipe1.addStep(step)
+            getInstruction(
+              recipe1.name,
+              3,
+              (step) => {
+                recipe1.addStep(step)
+                getInstruction(
+                  recipe1.name,
+                  4,
+                  (step) => {
+                    recipe1.addStep(step)
+                    recipe1.addSuccessText()
+                    recipe1.showImage()
+                  },
+                  (error) => console.log(error)
+                )
+              },
+              (error) => console.log(error)
+            )
+          },
+          (error) => console.log(error)
+        )
+      },
+      (error) => console.log(error)
+    )
+  },
+  (error) => console.log(error)
+)
 
 // Iteration 2 - using promises
-// ...
+const recipe2 = getRecipeMethods("steak", "Steak is ready!")
+obtainInstruction(recipe2.name, 0).then((step) => {
+  recipe2.addStep(step)
+  obtainInstruction(recipe2.name, 1).then((step) => {
+    recipe2.addStep(step)
+    obtainInstruction(recipe2.name, 2).then((step) => {
+      recipe2.addStep(step)
+      obtainInstruction(recipe2.name, 3).then((step) => {
+        recipe2.addStep(step)
+        obtainInstruction(recipe2.name, 4).then((step) => {
+          recipe2.addStep(step)
+          obtainInstruction(recipe2.name, 5).then((step) => {
+            recipe2.addStep(step)
+            obtainInstruction(recipe2.name, 6).then((step) => {
+              recipe2.addStep(step)
+              obtainInstruction(recipe2.name, 7).then((step) => {
+                recipe2.addStep(step)
+                recipe2.addSuccessText()
+                recipe2.showImage()
+              })
+            })
+          })
+        })
+      })
+    })
+  })
+})
 
 // Iteration 3 using async/await
-// ...
+const recipe3 = getRecipeMethods("broccoli", "Broccoli is ready!")
+async function makeBroccoli() {
+  const step0 = await obtainInstruction(recipe3.name, 0)
+  recipe3.addStep(step0)
+
+  const step1 = await obtainInstruction(recipe3.name, 1)
+  recipe3.addStep(step1)
+
+  const step2 = await obtainInstruction(recipe3.name, 2)
+  recipe3.addStep(step2)
+
+  const step3 = await obtainInstruction(recipe3.name, 3)
+  recipe3.addStep(step3)
+
+  const step4 = await obtainInstruction(recipe3.name, 4)
+  recipe3.addStep(step4)
+
+  const step5 = await obtainInstruction(recipe3.name, 5)
+  recipe3.addStep(step5)
+
+  const step6 = await obtainInstruction(recipe3.name, 6)
+  recipe3.addStep(step6)
+
+  recipe3.addSuccessText()
+  recipe3.showImage()
+}
+makeBroccoli()
 
 // Bonus 2 - Promise all
-// ...
+const recipe4 = getRecipeMethods(
+  "brusselsSprouts",
+  "Brussels sprouts are ready!"
+)
+const promises = Array.from({ length: brusselsSprouts.length }, (_, index) =>
+  obtainInstruction(recipe4.name, index)
+)
+Promise.all(promises).then((stepTexts) => {
+  stepTexts.forEach(recipe4.addStep)
+  recipe4.addSuccessText()
+  recipe4.showImage()
+})
--- javascript/obtainInstruction.js
+++ javascript/obtainInstruction.js
@@ -7,33 +7,26 @@ function obtainInstruction(food, step) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       // Get the instruction string
-      let instruction;
+      let instruction
 
       if (food === "mashedPotatoes") {
-        instruction = mashedPotatoes[step];
+        instruction = recipe1[step]
+      } else if (food === "steak") {
+        instruction = steak[step]
+      } else if (food === "brusselsSprouts") {
+        instruction = brusselsSprouts[step]
+      } else if (food === "broccoli") {
+        instruction = broccoli[step]
       }
-      else if (food === "steak") {
-        instruction = steak[step];
-      }
-      else if (food === "brusselsSprouts") {
-        instruction = brusselsSprouts[step];
-      }
-      else if (food === "broccoli") {
-        instruction = broccoli[step];
-      };
-      
 
       // Resolve or reject the promise
       if (!instruction) {
         reject("Instruction step does not exist!")
+      } else {
+        resolve(instruction)
       }
-      else {
-        resolve(instruction);
-      }
-
-    }, Math.floor(Math.random() * 1000));
-  });
+    }, Math.floor(Math.random() * 1000))
+  })
 }
-
 
 /***** ^^^ DO NOT TOUCH *****/
